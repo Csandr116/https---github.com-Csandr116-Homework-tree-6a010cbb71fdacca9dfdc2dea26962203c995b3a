c40a7be98068da56c1dae538966aa3347560e4b8
Csandr116 <150072079+Csandr116@users.noreply.github.com>
2023-12-19 22:30:53 +0300

    2.2

---
+++ script.js
@@ -0,0 +1,57 @@
+// Задание 1
+let a = 10;
+alert(a);
+a = 20;
+alert(a);
+// Задание 2
+const iphone = 2007;
+alert(iphone);
+// Задание 3
+const JSgod = "Brendan Eich";
+alert(JSgod);
+// Задание 4
+let num1 = 10;
+    num2 = 2;
+alert(num1+num2);
+alert(num1-num2);
+alert(num1*num2);
+alert(num1/num2);
+// Задание 5
+let result = 2**5;
+alert(result)
+// Задание 6
+ let a1 = 9;
+ let b = 2;
+ let remainder = a % b;
+ alert(remainder);
+// Задание 7
+let num = 1;
+num = num += 5;
+num = num -= 3;
+num = num *= 7;
+num = num /= 3;
+num = num ++ ;
+num = num -- ;
+alert(num);
+// Задание 8
+let age = prompt("Сколько вам лет?");
+alert(age);
+// Задание 9
+let user = {
+	name: "Tod",
+    age: 32,
+    isAdmin: true,
+    };
+// Задание 9.1
+user["city of resident"] = "NY-NefteUgansk";
+// Задание 9.2
+user.age = 33;
+// Задание 9.3
+delete user["city of resident"];
+// Задание 9.4
+let info = prompt("Какую информацию хотите узнать о пользователе?")
+alert(user[info]);
+// Задание 10
+let userName = prompt("как тебя зовут");
+alert('Привет, ${userName}');
+
